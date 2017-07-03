7959c1bf53c619c403342d0f523643af6c165b2e
Myvar <myvarhd@gmail.com>
2017-07-03 14:51:07 +0200

    init

---
+++ Apogee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Apogee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
