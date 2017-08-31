806ba7954922fddbcfb0a00223ce2a3f27cefb31
Myvarhd <myvarhd@gmail.com>
2017-08-31 12:51:57 +0200

    rendertarget work

--- Editor/mainscene.cs
+++ Editor/mainscene.cs
@@ -4,20 +4,30 @@ using Apogee.API;
 using Apogee.Core;
 using Apogee.Resources;
 using ImGuiNET;
+using OpenTK;
+using Vector2 = System.Numerics.Vector2;
+using Vector4 = System.Numerics.Vector4;
 
 
 public class MainScene : Scene
 {
     //3D
     private Camera Camera;
+
     private WorldObject monkey;
     private Shader BasicShader;
+    private RenderTarget MainView;
 
     public override void Load()
     {
         Camera = new Camera(0.1f, 1000, Engine.Window.Width, Engine.Window.Height, 70, 10);
         BasicShader = new Shader("./Shaders/basic");
         monkey = new WorldObject(Engine.Assets.Load<Model>("monkey.ogex"));
+        monkey.Transform.RotX = 90;
+        monkey.Transform.RotZ = 180;
+        monkey.Transform.Z = 3;
+        MainView = new RenderTarget(800, 600);
+        Engine.Window.WindowState = WindowState.Maximized;
     }
 
     public override void Update(double deltaTime)
@@ -40,10 +50,23 @@ public class MainScene : Scene
         }
 
         ImGui.EndWindow();
+
+
+        ImGui.BeginWindow("MainViewPort", WindowFlags.AlwaysAutoResize);
+
+        int x = MainView.Texture;
+        ImGui.Image(new IntPtr(x), new Vector2(800, 600), new Vector2(0, 1), new Vector2(1, 0), new Vector4(1, 1, 1, 1),
+            new Vector4(0, 0, 0, 0));
+
+        ImGui.EndWindow();
     }
 
     public override void Draw()
     {
+        MainView.Bind();
+
         monkey.Draw(BasicShader, Camera);
+
+        MainView.BindMainWindow();
     }
 }
